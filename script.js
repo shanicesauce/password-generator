511635dbc58e163a0ddc1a3bc12c8591efba0c6e
shanicesauce <shanicesauceda@gmail.com>
2022-08-01 22:13:00 -0500

    successfully made loop if not 8-12 characters

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
 // // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var length = document.querySelector("#length");
-var upperEl = document.querySelector("#uppercase");
-var lowerEl = document.querySelector("#lower");
-var specialEl = document.querySelector("#special");
-var numberEl = document.querySelector("#number");
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var special = ["!", "”","#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "{", "|", "}",, "~", "[","]", ]; 
+var number = ["0","1","2","3","4","5","6","7","8","9"]
 
 // Assignment code here
-var criteria= {
-  lower : getRandomLower,
-  upper : getRandomUpper,
-  number : getRandomNumber,
-  symbol : getRandomSymbol,
-}
+var getRandomLower= Math.floor(Math.random() * lowercase.length);
+    console.log (getRandomLower,lowercase[getRandomLower]);
+    //lowercase[getRandomLower] will exicute only once 
+var getRandomUpper = Math.floor(Math.random() * uppercase.length);
+    console.log (getRandomUpper,uppercase[getRandomUpper]);
+var getRandomNumber = Math.floor(Math.random() * number.length);
+    console.log (getRandomNumber,number[getRandomNumber]);
+var getRandomSymbol = [Math.floor(Math.random() * special.length)];
+    console.log (getRandomSymbol,special[getRandomSymbol]);
+
+
+
+ 
 
 // // Write password to the #password input
-function writePassword() {
+function writePassword() {  
+  characters = window.prompt ("How many characters would you like?");
+
+  console.log(characters,typeof characters);
+
+  var length = parseInt(characters);
+
+  console.log(characters,typeof length);
+
+  if (length < 8 || length > 128) {
+    window.alert ("Must choose between 8 - 128."); 
+    return writePassword();
+  };
+  lowercase = window.confirm ("Would you like to include lowercase?");
+  uppercase = window.confirm ("Would you like to include upper case?");
+  special = window.confirm ("Would you like to include a special character?");
+  number = window.confirm ("Would you like to include a number?");
+  for (var i=0; i < length; i++)   {
+//if selected then include element that was selected
+//if was selected go into array depends on amount of characters
+//lowercase[getRandomLower]
+  };
+ 
+ 
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -24,27 +55,15 @@ function writePassword() {
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword) 
-{
+
+  // click button then generated prompts for password criteria options
+  var generatePassword = {
+    getRandomLower,
+    getRandomUpper,
+    getRandomNumber,
+    getRandomSymbol
+  };
+
   
-};
-generatePassword (checkLower,checkNumb,checkSpec,checkUpper);
-// click button then generated prompts for password criteria options
-function getRandomLower(){
-  var lowercase = ("abcdefghijklmnopqrstuvwxyz");
-  return lowercase[Math.floor(Math.random() * lowercase.length)];
-
-}
-
-function getRandomUpper(){
-  var uppercase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-  return uppercase[Math.floor(Math.random() * uppercase.length)];
-}
-
-function getRandomNumber(){
-  var number = ("0 1 2 3 4 5 6 7 8 9");
-  return number[Math.floor(Math.random() * number.length)];
-}
-function getRandomSymbol(){
-  var symbol = (" ! ” # $ % & ’ ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~");
-  return symbol[Math.floor(Math.random() * symbol.length)];
-}
+  
+ 
