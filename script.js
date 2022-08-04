77c7ce3a858d6063ed05a1da4038ccf66b6b83b8
shanicesauce <shanicesauceda@gmail.com>
2022-08-03 22:05:04 -0500

    completed loop

--- script.js
+++ script.js
@@ -2,23 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var special = ["!", "”","#", "$", "%", "&", "’", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "^", "{", "|", "}",, "~", "[","]", ]; 
+var special = ["\!", "\”","\#", "\$", "\%", "\&", "\’", "\(", "\)", "\*", "\+", "\,", "\-", "\.", "\/", "\:", "\;", "\<", "\=", "\>", "\?", "\@", "\^", "\{", "\|", "\}",, "\~", "\[","\]",]; 
 var number = ["0","1","2","3","4","5","6","7","8","9"]
 
 // Assignment code here
-var getRandomLower= Math.floor(Math.random() * lowercase.length);
-    console.log (getRandomLower,lowercase[getRandomLower]);
 
-var getRandomUpper = Math.floor(Math.random() * uppercase.length);
-    console.log (getRandomUpper,uppercase[getRandomUpper]);
-
-var getRandomNumber = Math.floor(Math.random() * number.length);
-    console.log (getRandomNumber,number[getRandomNumber]);
-
-var getRandomSymbol = [Math.floor(Math.random() * special.length)];
-    console.log (getRandomSymbol,special[getRandomSymbol]);
-
- 
 
 // // Write password to the #password input
  function writePassword(){  
@@ -34,27 +22,38 @@ var getRandomSymbol = [Math.floor(Math.random() * special.length)];
     window.alert ("Must choose between 8 - 128."); 
     return writePassword();
   };
-  lowercase = window.confirm ("Would you like to include lowercase?");
-  uppercase = window.confirm ("Would you like to include upper case?");
-  special = window.confirm ("Would you like to include a special character?");
-  number = window.confirm ("Would you like to include a number?");
-  for (var i=0; i < length; i++)   {
-    String.fromCharCode (lowercase[getRandomLower]).textContent;
-    String.fromCharCode (uppercase[getRandomUpper]).textContent;
-    String.fromCharCode (special[getRandomSymbol]).textContent;
-    String.fromCharCode (number[getRandomNumber]).textContent;
-//if selected then include elements that was selected
+  lowercaseConfirm = window.confirm ("Would you like to include lowercase?");
+  uppercaseConfirm = window.confirm ("Would you like to include upper case?");
+  specialConfirm = window.confirm ("Would you like to include a special character?");
+  numberConfirm = window.confirm ("Would you like to include a number?");
+  //if selected then include elements that was selected
 //if was selected go into array depends on amount of characters
-
-
-  var password = generatePassword();
-  var passwordText = document.getElementById("#password");
-
-  passwordText.value = password;
-
+var password = ""
+ for (var i=0; i < length; i++)   {
+if (lowercaseConfirm === true) {
+ password += lowercase[Math.floor(Math.random() * lowercase.length)];
 }
+if (uppercaseConfirm === true) {
+  password += uppercase[Math.floor(Math.random() * uppercase.length)];
+ }
+ if (specialConfirm === true) {
+  password += number[Math.floor(Math.random() * number.length)];
+ }
+ if (numberConfirm === true) {
+  password += special[Math.floor(Math.random() * special.length)];
+ }
+console.log(password);
+
+
+  }
+  var passwordText = document.querySelector("#password");
+  console.log(passwordText);
+  passwordText.value = password
+
 };
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword) 
 
   // click button then generated prompts for password criteria options
+
+  // passwordText.value = password;
